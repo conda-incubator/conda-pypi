819a6a33586950f7e0ab95c316e99764b40a648f
jaimergp <jaimergp@users.noreply.github.com>
2023-12-19 12:39:25 +0100

    add test with version

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,13 @@
 import sys
 
+import pytest
+
 from conda.testing import CondaCLIFixture, TmpEnvFixture
 from conda.testing.integration import package_is_installed
 
-def test_conda_pip_install_numpy(tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixture):
+
+@pytest.mark.parametrize("spec", ["numpy", "numpy=1.20"])
+def test_conda_pip_install_numpy(tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixture, spec: str):
     with tmp_env("python=3.9", "pip") as prefix:
         out, err, rc = conda_cli(
             "pip",
@@ -11,10 +15,10 @@ def test_conda_pip_install_numpy(tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixt
             prefix,
             "--yes",
             "install",
-            "numpy",
+            spec,
         )
         print(out)
         print(err, file=sys.stderr)
         assert rc == 0
-        assert "numpy" in out
-        assert package_is_installed(str(prefix), "numpy")
+        assert spec in out
+        assert package_is_installed(str(prefix), spec)
