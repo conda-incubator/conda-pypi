ab347d526fae34ae81ea219e96e134e40d346a74
jaimergp <jaimergp@users.noreply.github.com>
2024-04-05 10:46:31 +0200

    fix decoding issues on WIndows

--- conda_pip/dependencies/pip.py
+++ conda_pip/dependencies/pip.py
@@ -1,10 +1,11 @@
 import json
-from logging import DEBUG, getLogger
+import os
+from logging import getLogger
 from collections import defaultdict
 from subprocess import run
+from tempfile import NamedTemporaryFile
 
 from conda.exceptions import CondaError
-from grayskull.base.pkg_info import is_pkg_available
 
 from ..utils import get_env_python
 
@@ -12,39 +13,47 @@ logger = getLogger(f"conda.{__name__}")
 
 
 def _analyze_with_pip(
-    *packages,
-    prefer_on_conda=True,
-    channel="conda-forge",
-    prefix=None,
-    force_reinstall=False,
-):
+    *packages: str,
+    prefix: str | None = None,
+    force_reinstall: bool = False,
+) -> tuple[dict[str, list[str]], dict[str, list[str]]]:
+    # pip can output to stdout via `--report -` (dash), but this
+    # creates issues on Windows due to undecodable characters on some
+    # project descriptions (e.g. charset-normalizer, amusingly), which
+    # makes pip crash internally. Probably a bug on their end.
+    # So we use a temporary file instead to work with bytes.
+    json_output = NamedTemporaryFile(suffix=".json", delete=False)
+    json_output.close()  # Prevent access errors on Windows
+
     cmd = [
-        get_env_python(prefix),
+        str(get_env_python(prefix)),
         "-mpip",
         "install",
         "--dry-run",
         "--ignore-installed",
         *(("--force-reinstall",) if force_reinstall else ()),
         "--report",
-        "-",  # this tells pip to print the report to stdout
-        "--quiet",  # this is needed so normal pip output doesn't get mixed with json
+        json_output.name,
         *packages,
     ]
-    process = run(cmd, capture_output=True, text=True, errors="replace")
+    process = run(cmd, capture_output=True, text=True)
     if process.returncode != 0:
         raise CondaError(
             f"Failed to analyze dependencies with pip:\n"
-            f"  command: {' '.join(map(str, cmd))}\n"
+            f"  command: {' '.join(cmd)}\n"
             f"  exit code: {process.returncode}\n"
             f"  stderr:\n{process.stderr}\n"
             f"  stdout:\n{process.stdout}\n"
         )
-    logger.debug(
-        "pip (%s) provided the following report:\n%s",
-        " ".join(map(str, cmd)),
-        process.stdout,
-    )
-    report = json.loads(process.stdout)
+    logger.debug("pip (%s) provided the following report:\n%s", " ".join(cmd), process.stdout)
+
+    with open(json_output.name, "rb") as f:
+        # We need binary mode because the JSON output might
+        # contain weird unicode stuff (as part of the project
+        # description or README).
+        report = json.loads(f.read())
+    os.unlink(json_output.name)
+
     deps_from_pip = defaultdict(list)
     conda_deps = defaultdict(list)
     for item in report["install"]:
