6859cbaaab83f252684d1a4ecd51c42e86903522
Daniel Holth <dholth@fastmail.fm>
2024-07-20 20:03:07 -0400

    indexable package

--- dev2conda/tarconda.py
+++ dev2conda/tarconda.py
@@ -1,7 +1,7 @@
 import tarfile
 import tempfile
 from pathlib import Path
-import os.path
+import time
 
 from conda_package_streaming.transmute import transmute_stream
 
@@ -26,3 +26,17 @@ def create(source, destination):
             transmute_stream(
                 "someconda", destination, package_stream=((tar, entry) for entry in tar)
             )
+
+
+def index_json(name, version="0.0.0", subdir="noarch", depends=()):
+    return {
+        "build": "0",
+        "build_number": 0,
+        "depends": list(depends),
+        "license": "",
+        "license_family": "",
+        "name": name,
+        "subdir": subdir,
+        "timestamp": time.time_ns() / 1000000,
+        "version": version,
+    }
