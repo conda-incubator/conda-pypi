cb27e3dcd08036d626054b270f1de5dca19b22af
Daniel Holth <dholth@fastmail.fm>
2024-07-22 16:08:33 -0400

    create paths.json

--- tests/test_tarconda.py
+++ tests/test_tarconda.py
@@ -1,11 +1,16 @@
-from pathlib import Path
-
-from dev2conda.tarconda import create, index_json
-import zipfile
-import shutil
-from subprocess import run
-import sys
+import hashlib
 import json
+import os
+import shutil
+import sys
+import zipfile
+from pathlib import Path
+from subprocess import run
+
+from conda.cli.main import main_subshell
+
+from dev2conda.conda_build_utils import PathType, sha256_checksum
+from dev2conda.build import builder, create, filter, index_json, paths_json
 
 here = Path(__file__).parent
 
@@ -19,16 +24,82 @@ def test_tarconda(tmp_path):
 def test_indexable(tmp_path):
     # tmp_path = Path("/tmp/test")
     # tmp_path.mkdir()
-    dest = tmp_path / "somepackage"
     noarch = tmp_path / "noarch"
     noarch.mkdir()
-    shutil.copytree(here.parent, dest, ignore=shutil.ignore_patterns(".git"))
-    (dest / "info").mkdir()
-    (dest / "info" / "index.json").write_text(json.dumps(index_json("somepackage")))
 
-    create(dest, noarch)
+    NAME = "somepackage"
+    VERSION = "1.0"
+
+    record = index_json(NAME, VERSION)
+
+    file_id = f"{record['name']}-{record['version']}-{record['build']}"
+
+    dest = tmp_path / file_id
+
+    shutil.copytree(here.parent, dest, ignore=shutil.ignore_patterns(".git"))
+
+    (dest / "info").mkdir()
+    (dest / "info" / "index.json").write_text(json.dumps(record))
+
+    paths = paths_json(dest)
+
+    (dest / "info" / "paths.json").write_text(json.dumps(paths))
+
+    with builder(noarch, file_id) as tar:
+        tar.add(dest, "", filter=filter)
 
     # can cause a circular import in requests if charset-normalizer but not chardet is installed
     run([sys.executable, "-m", "conda_index", str(tmp_path)], check=True)
     repodata = json.loads((noarch / "repodata.json").read_text())
     assert repodata["packages.conda"]
+
+    # name, version, build = dist_str.rsplit("-", 2) must be named like this
+    dest_package = noarch / f"{file_id}.conda"
+    assert dest_package.exists()
+
+    main_subshell("create", "--prefix", str(tmp_path / "env"), str(dest_package))
+
+    # in conda-meta
+    example_conda_meta = {
+        "build": "0",
+        "build_number": 0,
+        "channel": "file:///tmp/pytest-of-dholth/pytest-50/test_indexable0/noarch",
+        "constrains": [],
+        "depends": [],
+        "extracted_package_dir": "/home/dholth/miniconda3/pkgs/somepackage-1.0-0",
+        "files": [],  # will contain failes
+        "fn": "somepackage-1.0-0.conda",
+        "license": "",
+        "license_family": "",
+        "link": {"source": "/home/dholth/miniconda3/pkgs/somepackage-1.0-0", "type": 1},
+        "md5": "c551ede58a0344516e1b803de10ff5c8",
+        "name": "somepackage",
+        "package_tarball_full_path": "/home/dholth/miniconda3/pkgs/somepackage-1.0-0.conda",
+        "paths_data": {
+            "paths": [],
+            "paths_version": 1,
+        },  # duplicates "files" list? plus checksums
+        "requested_spec": "test_indexable0/noarch::somepackage==1.0=0",
+        "sha256": "ba7741b7ce1470357397f6f8f4d0467be8d4162006e5a87e276c28d3b2cfbc14",
+        "size": 79824,
+        "subdir": "noarch",
+        "timestamp": 1721655445630,
+        "url": "file:///tmp/pytest-of-dholth/pytest-50/test_indexable0/noarch/somepackage-1.0-0.conda",
+        "version": "1.0",
+    }
+
+    conda_meta = json.loads(
+        (tmp_path / "env" / "conda-meta" / f"{file_id}.json").read_text()
+    )
+    assert len(conda_meta["files"])
+
+
+def test_path_type():
+    assert PathType.hardlink.__json__() == str(PathType.hardlink)
+
+
+def test_checksum(tmp_path):
+    (tmp_path / "nowhere").symlink_to(tmp_path / "missing")
+    assert sha256_checksum(tmp_path / "nowhere") == hashlib.sha256().hexdigest()
+    os.mkfifo(tmp_path / "fifo")
+    assert sha256_checksum(tmp_path / "fifo") is None
