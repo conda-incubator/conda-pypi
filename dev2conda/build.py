ac27eedf79db465057c663f6a603bd679e9377b4
Daniel Holth <dholth@fastmail.fm>
2024-07-22 16:19:14 -0400

    support symlink-to-nowhere

--- dev2conda/build.py
+++ dev2conda/build.py
@@ -68,8 +68,6 @@ def index_json(
 
 
 # see conda_build.build.build_info_files_json_v1
-
-
 def paths_json(base: Path | str):
     """
     Build simple paths.json with only 'hardlink' or 'symlink' types.
@@ -94,13 +92,18 @@ def _paths(base, path, filter=lambda x: x.name != ".git"):
         if entry.is_dir():
             yield from _paths(base, entry.path, filter=filter)
         elif entry.is_file() or entry.is_symlink():
+            try:
+                st_size = entry.stat().st_size
+            except FileNotFoundError:
+                st_size = 0  # symlink to nowhere
             yield {
                 "_path": relative_path,
                 "path_type": str(
                     PathType.softlink if entry.is_symlink() else PathType.hardlink
                 ),
                 "sha256": sha256_checksum(entry.path, entry),
-                "size_in_bytes": entry.stat().st_size,
+                "size_in_bytes": st_size,
             }
         else:
-            print("Not regular file", entry)  # pragma: no cover
+            print("Not regular file", entry)
+            # will Python's tarfile add pipes, device nodes to the archive?
