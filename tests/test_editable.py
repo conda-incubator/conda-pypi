b0d23bfe619370b1fdd3c197eb4ef6f84630003e
Daniel Holth <dholth@fastmail.fm>
2024-08-16 14:31:27 -0400

    test against pypa/build test packages

--- tests/test_editable.py
+++ tests/test_editable.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from conda_pupa.editable import editable, normalize
 
 
@@ -9,3 +11,18 @@ def test_editable():
 
 def test_normalize():
     assert normalize("flit_core") == "flit-core"
+
+
+def pypa_build_packages():
+    """
+    Test packages from pypa/build repository.
+
+    (Clone pypa/build into tests/)
+    """
+    here = Path(__file__).parent
+    return list(Path(here, "build", "tests", "packages").glob("*"))
+
+
+@pytest.mark.parametrize("package", pypa_build_packages())
+def test_editable_from_build(package):
+    editable(package)
