62f35af82dcd6f1acd23ad7b11dcc2146402d8e9
Ryan Keith <rkeith@anaconda.com>
2025-09-05 15:38:47 -0700

    Removing unnecessary functions and test.

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -12,37 +12,6 @@ from pytest_mock import MockerFixture
 from conda_pypi.python_paths import get_env_python, get_current_externally_managed_path
 
 
-def test_pip_required_in_target_env(
-    tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixture, monkeypatch
-):
-    monkeypatch.setenv("CONDA_ADD_PIP_AS_PYTHON_DEPENDENCY", "false")
-    reset_context()
-    with tmp_env("xz") as prefix:
-        args = ("pypi", "-p", prefix, "--yes", "install", "requests")
-
-        with pytest.raises(CondaError, match="requires python"):
-            out, err, rc = conda_cli(*args)
-        out, err, rc = conda_cli("install", "-p", prefix, "--yes", "python=3.9")
-        PrefixData._cache_.clear()  # clear cache to force re-read of prefix
-        assert package_is_installed(str(prefix), "python=3.9")
-        assert not package_is_installed(str(prefix), "pip")
-        PrefixData._cache_.clear()
-
-        with pytest.raises(CondaError, match="requires pip"):
-            out, err, rc = conda_cli(*args)
-        out, err, rc = conda_cli("install", "-p", prefix, "--yes", "pip")
-        PrefixData._cache_.clear()  # clear cache to force re-read of prefix
-        assert package_is_installed(str(prefix), "pip")
-        PrefixData._cache_.clear()
-
-        out, err, rc = conda_cli(*args)
-        PrefixData._cache_.clear()
-        assert rc == 0
-        assert package_is_installed(str(prefix), "requests")
-    monkeypatch.undo()
-    reset_context()
-
-
 def test_externally_managed(
     tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixture, monkeypatch: MockerFixture
 ):
