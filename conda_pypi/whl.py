86185ddd834e04d6d52ea6db5234eb3c928cf8c8
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-11-03 22:18:20 +0100

    make tests pass

--- conda_pypi/whl.py
+++ conda_pypi/whl.py
@@ -1,6 +1,54 @@
 import logging
+import re
+from conda.base.context import context
+from conda.base.constants import CONDA_PACKAGE_EXTENSION_V1, CONDA_PACKAGE_EXTENSION_V2
 
 log = logging.getLogger(__name__)
+
+def mocked_from_dist_str(cls, dist_str):
+        parts = {}
+        if dist_str[-len(CONDA_PACKAGE_EXTENSION_V2) :] == CONDA_PACKAGE_EXTENSION_V2:
+            dist_str = dist_str[: -len(CONDA_PACKAGE_EXTENSION_V2)]
+        elif dist_str[-len(CONDA_PACKAGE_EXTENSION_V1) :] == CONDA_PACKAGE_EXTENSION_V1:
+            dist_str = dist_str[: -len(CONDA_PACKAGE_EXTENSION_V1)]
+        elif dist_str[-4:]==".whl":
+            dist_str=dist_str[: -4]
+        if "::" in dist_str:
+            channel_subdir_str, dist_str = dist_str.split("::", 1)
+            if "/" in channel_subdir_str:
+                channel_str, subdir = channel_subdir_str.rsplit("/", 1)
+                if subdir not in context.known_subdirs:
+                    channel_str = channel_subdir_str
+                    subdir = None
+                parts["channel"] = channel_str
+                if subdir:
+                    parts["subdir"] = subdir
+            else:
+                parts["channel"] = channel_subdir_str
+
+        name, version, build = dist_str.rsplit("-", 2)
+        parts.update(
+            {
+                "name": name,
+                "version": version,
+                "build": build,
+            }
+        )
+        return cls(**parts)
+
+def mocked_is_package_file(path):
+    print("mocked func called")
+    return path[-6:] == ".conda" or path[-8:] == ".tar.bz2" or path[-4:]==".whl"
+
+mocked_url_pat=re.compile(
+    r"(?:(?P<url_p>.+)(?:[/\\]))?"
+    r"(?P<fn>[^/\\#]+(?:\.tar\.bz2|\.conda|\.whl))"
+    r"(?:#("
+    r"(?P<md5>[0-9a-f]{32})"
+    r"|((sha256:)?(?P<sha256>[0-9a-f]{64}))"
+    r"))?$"
+)
+
 
 
 def add_whl_support(command: str) -> None:
@@ -26,6 +74,7 @@ def add_whl_support(command: str) -> None:
     if conda.core.path_actions.extract_tarball.__module__ != __name__:
         from conda_pypi.pre_command.extract_whl_or_tarball import extract_whl_or_tarball
 
+        print("mocking extract_tarball_whl_support")
         conda.core.path_actions.extract_tarball = extract_whl_or_tarball
 
     # Allow the creation of prefix record JSON files for .whl files
@@ -41,4 +90,12 @@ def add_whl_support(command: str) -> None:
 
         PrefixData._load_single_record = _load_single_record
 
+    import conda.models.match_spec
+    conda.models.match_spec.is_package_file=mocked_is_package_file
+
+    import conda.misc
+    conda.misc.url_pat = mocked_url_pat
+
+    import conda.models.match_spec
+    conda.models.match_spec.MatchSpec.from_dist_str = mocked_from_dist_str
     return
