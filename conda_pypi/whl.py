dc2ca4c6b782f66905985c3f756cf2d99c36aedb
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-11-05 15:17:58 +0100

    add tests

--- conda_pypi/whl.py
+++ conda_pypi/whl.py
@@ -37,11 +37,9 @@ def add_whl_support(command: str) -> None:
     # Patch the extract_tarball function
     # Add support for extracting wheels with in-line creation of conda metadata files
     import conda.core.path_actions
+    from conda_pypi.pre_command.extract_whl_or_tarball import extract_whl_or_tarball
 
-    if conda.core.path_actions.extract_tarball.__module__ != __name__:
-        from conda_pypi.pre_command.extract_whl_or_tarball import extract_whl_or_tarball
-
-        conda.core.path_actions.extract_tarball = extract_whl_or_tarball
+    conda.core.path_actions.extract_tarball = extract_whl_or_tarball
 
     # Allow the creation of prefix record JSON files for .whl files
     import conda.core.prefix_data
@@ -50,11 +48,9 @@ def add_whl_support(command: str) -> None:
 
     # Skip the check that name, version, build matches filename in prefix record json
     from conda.core.prefix_data import PrefixData
+    from conda_pypi.pre_command.patched_load import _load_single_record
 
-    if PrefixData._load_single_record.__module__ != __name__:
-        from conda_pypi.pre_command.patched_load import _load_single_record
-
-        PrefixData._load_single_record = _load_single_record
+    PrefixData._load_single_record = _load_single_record
 
     import conda.models.match_spec
 
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -4,22 +4,41 @@ from conda_pypi.pre_command import extract_whl_or_tarball
 from conda_pypi.pre_command.extract_whl import extract_whl_as_conda_pkg
 from conda_pypi import plugin
 
+import pytest
 from pathlib import Path
 
-WHL_URL = "https://files.pythonhosted.org/packages/45/7f/0e961cf3908bc4c1c3e027de2794f867c6c89fb4916fc7dba295a0e80a2d/boltons-25.0.0-py3-none-any.whl"
+WHL_HTTP_URL = "https://files.pythonhosted.org/packages/45/7f/0e961cf3908bc4c1c3e027de2794f867c6c89fb4916fc7dba295a0e80a2d/boltons-25.0.0-py3-none-any.whl"
 
 
+@pytest.mark.parametrize(
+    "package",
+    [WHL_HTTP_URL, "{file}", "file://{file}"],
+)
 def test_extract_whl_as_conda_called(
     tmp_env: TmpEnvFixture,
     conda_cli: CondaCLIFixture,
     mocker: MockerFixture,
     pypi_demo_package_wheel_path: Path,
     tmp_pkgs_dir: Path,
+    package,
+):
+    package = package.format(file=pypi_demo_package_wheel_path)
+    with tmp_env("python") as prefix:
+        spy = mocker.spy(extract_whl_or_tarball.extract_whl, "extract_whl_as_conda_pkg")
+        conda_cli("install", package, "-p", prefix)
+        spy.assert_called_once()
+
+
+def test_extract_whl_as_conda_not_called(
+    tmp_env: TmpEnvFixture,
+    conda_cli: CondaCLIFixture,
+    mocker: MockerFixture,
+    tmp_pkgs_dir: Path,
 ):
     with tmp_env("python") as prefix:
         spy = mocker.spy(extract_whl_or_tarball.extract_whl, "extract_whl_as_conda_pkg")
-        conda_cli("install", pypi_demo_package_wheel_path, "-p", prefix)
-        spy.assert_called_once()
+        conda_cli("install", "numpy", "-p", prefix, "--yes")
+        spy.assert_not_called()
 
 
 def test_plugin_invoked(
