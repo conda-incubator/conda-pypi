44d032e9a1b1f2443515108747a3eeed5957f0f3
Ken Odegard <kodegard@anaconda.com>
2025-11-05 21:19:57 +0100

    Combine tests to avoid redundancy

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -3,61 +3,59 @@ from pytest_mock import MockerFixture
 from conda_pypi.pre_command import extract_whl_or_tarball
 from conda_pypi.pre_command.extract_whl import extract_whl_as_conda_pkg
 from conda_pypi import plugin
-
 import pytest
 from pathlib import Path
 
+
 WHL_HTTP_URL = "https://files.pythonhosted.org/packages/45/7f/0e961cf3908bc4c1c3e027de2794f867c6c89fb4916fc7dba295a0e80a2d/boltons-25.0.0-py3-none-any.whl"
+CONDA_URL = "https://repo.anaconda.com/pkgs/main/osx-arm64/boltons-25.0.0-py314hca03da5_0.conda"
 
 
 @pytest.mark.parametrize(
-    "package",
-    [WHL_HTTP_URL, "{file}", "file://{file}"],
+    "package,call_count",
+    [
+        pytest.param(WHL_HTTP_URL, 1, id=".whl url"),
+        pytest.param("{file}", 1, id=".whl file"),
+        pytest.param("file:///{file}", 1, id=".whl file url"),
+        pytest.param(CONDA_URL, 0, id=".conda url"),
+    ],
 )
 def test_extract_whl_as_conda_called(
     tmp_env: TmpEnvFixture,
     conda_cli: CondaCLIFixture,
     mocker: MockerFixture,
     pypi_demo_package_wheel_path: Path,
-    tmp_pkgs_dir: Path,
-    package,
+    tmp_pkgs_dir: Path,  # use empty package cache directory
+    tmp_path: Path,
+    package: str,
+    call_count: int,
 ):
     package = package.format(file=pypi_demo_package_wheel_path)
-    with tmp_env("python") as prefix:
-        spy = mocker.spy(extract_whl_or_tarball.extract_whl, "extract_whl_as_conda_pkg")
-        conda_cli("install", package, "-p", prefix)
-        spy.assert_called_once()
+    with tmp_env() as prefix:
+        # mock python installed in prefix
+        mocker.patch(
+            "conda.core.link.UnlinkLinkTransaction._get_python_info",
+            return_value=("3.10", str(tmp_path)),
+        )
 
+        # spy on monkeypatches
+        spy_add_whl_support = mocker.spy(plugin, "add_whl_support")
+        spy_extract_whl_as_conda_pkg = mocker.spy(
+            extract_whl_or_tarball.extract_whl, "extract_whl_as_conda_pkg"
+        )
 
-def test_extract_whl_as_conda_not_called(
-    tmp_env: TmpEnvFixture,
-    conda_cli: CondaCLIFixture,
-    mocker: MockerFixture,
-    tmp_pkgs_dir: Path,
-):
-    with tmp_env("python") as prefix:
-        spy = mocker.spy(extract_whl_or_tarball.extract_whl, "extract_whl_as_conda_pkg")
-        conda_cli("install", "numpy", "-p", prefix, "--yes")
-        spy.assert_not_called()
+        # install package
+        conda_cli("install", f"--prefix={prefix}", package)
 
+        # monkeypatch always happens
+        assert spy_add_whl_support.call_count == 1
 
-def test_plugin_invoked(
-    tmp_env: TmpEnvFixture,
-    conda_cli: CondaCLIFixture,
-    mocker: MockerFixture,
-    pypi_demo_package_wheel_path: Path,
-):
-    with tmp_env("python") as prefix:
-        spy = mocker.spy(plugin, "add_whl_support")
-        conda_cli("install", pypi_demo_package_wheel_path, "-p", prefix)
-        spy.assert_called_once()
+        # wheel extraction only happens for .whl
+        assert spy_extract_whl_as_conda_pkg.call_count == call_count
 
 
 def test_extract_whl_as_conda_pkg(
-    tmp_env: TmpEnvFixture,
-    conda_cli: CondaCLIFixture,
-    mocker: MockerFixture,
     pypi_demo_package_wheel_path: Path,
+    tmp_path: Path,
 ):
-    with tmp_env("python") as prefix:
-        extract_whl_as_conda_pkg(pypi_demo_package_wheel_path, prefix)
+    extract_whl_as_conda_pkg(pypi_demo_package_wheel_path, tmp_path)
