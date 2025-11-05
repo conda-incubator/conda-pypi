5801f1a2724d4ceb072b01f3eb73b3560a9442a4
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-11-05 12:58:34 +0100

    remove unused code and debug statements

--- conda_pypi/pre_command/extract_whl_or_tarball.py
+++ conda_pypi/pre_command/extract_whl_or_tarball.py
@@ -7,7 +7,6 @@ def extract_whl_or_tarball(
     target_full_path=None,
     progress_update_callback=None,
 ):
-    print("Running extract_whl_tarball", source_full_path)
     if source_full_path.endswith(".whl"):
         return extract_whl.extract_whl_as_conda_pkg(
             source_full_path,
--- conda_pypi/whl.py
+++ conda_pypi/whl.py
@@ -1,41 +1,7 @@
 import logging
 import re
-from conda.base.context import context
-from conda.base.constants import CONDA_PACKAGE_EXTENSION_V1, CONDA_PACKAGE_EXTENSION_V2
 
 log = logging.getLogger(__name__)
-
-
-def mocked_from_dist_str(cls, dist_str):
-    parts = {}
-    if dist_str[-len(CONDA_PACKAGE_EXTENSION_V2) :] == CONDA_PACKAGE_EXTENSION_V2:
-        dist_str = dist_str[: -len(CONDA_PACKAGE_EXTENSION_V2)]
-    elif dist_str[-len(CONDA_PACKAGE_EXTENSION_V1) :] == CONDA_PACKAGE_EXTENSION_V1:
-        dist_str = dist_str[: -len(CONDA_PACKAGE_EXTENSION_V1)]
-    elif dist_str[-4:] == ".whl":
-        dist_str = dist_str[:-4]
-    if "::" in dist_str:
-        channel_subdir_str, dist_str = dist_str.split("::", 1)
-        if "/" in channel_subdir_str:
-            channel_str, subdir = channel_subdir_str.rsplit("/", 1)
-            if subdir not in context.known_subdirs:
-                channel_str = channel_subdir_str
-                subdir = None
-            parts["channel"] = channel_str
-            if subdir:
-                parts["subdir"] = subdir
-        else:
-            parts["channel"] = channel_subdir_str
-
-    name, version, build = dist_str.rsplit("-", 2)
-    parts.update(
-        {
-            "name": name,
-            "version": version,
-            "build": build,
-        }
-    )
-    return cls(**parts)
 
 
 def mocked_is_package_file(path):
@@ -98,6 +64,7 @@ def add_whl_support(command: str) -> None:
 
     conda.misc.url_pat = mocked_url_pat
 
-    # import conda.models.match_spec
-    # conda.models.match_spec.MatchSpec.from_dist_str = mocked_from_dist_str
+    # TODO
+    # There is some extension handling taking place in `conda.models.match_spec.MatchSpec.from_dist_str``
+    # that we might need to patch
     return
